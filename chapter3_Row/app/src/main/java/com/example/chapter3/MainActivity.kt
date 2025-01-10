dc1e6570e8f54a091141fcdf6ca4e21d020e6fef
wjlee <000jun2626@gmail.com>
2025-01-10 22:38:32 +0900

    chapter3(Row): first

--- chapter3_Row/app/src/main/java/com/example/chapter3/MainActivity.kt
+++ chapter3_Row/app/src/main/java/com/example/chapter3/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.chapter3.ui.theme.Chapter3Theme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Chapter3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                    Container()
+
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Container(){
+    Row() {
+        DummyBox()
+        DummyBox()
+        DummyBox()
+    }
 }
+
+@Composable
+fun DummyBox(modifier: Modifier = Modifier) {
+    val red = Random.nextInt(256)
+    val green = Random.nextInt(256)
+    val blue = Random.nextInt(256)
+    val randomColor = Color(red, green, blue)
+    Box(modifier = modifier
+        .size(100.dp)
+        .background(randomColor)) {
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Chapter3Theme {
-        Greeting("Android")
+        Container()
     }
 }
