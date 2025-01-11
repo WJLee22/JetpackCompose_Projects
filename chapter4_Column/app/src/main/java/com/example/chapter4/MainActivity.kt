3dbd9d4f600a140a8bc18eaa84e479cfd4749e1c
wjlee <000jun2626@gmail.com>
2025-01-12 02:59:41 +0900

    chapter4(Column): first Commit

---
+++ chapter4_Column/app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.chapter4
+
+import android.graphics.ColorSpace.Rgb
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chapter4.ui.theme.Chapter4Theme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Chapter4Theme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Chapter4Theme {
+
+    }
+}
